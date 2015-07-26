d33e6e4e1ee2e844dd17f3c7be22070a1dfe31c6
Sergiu Grabovschi <sergiu.grabovschi@endava.com>
2015-07-26 21:27:49 +0300

    Added Console App

--- IService1.cs
+++ IService1.cs
@@ -20,8 +20,7 @@ namespace LEDApp
         // TODO: Add your service operations here
     }
 
-    // Use a data contract as illustrated in the sample below to add composite types to service operations.
-    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "LEDApp.ContractType".
+    
     [DataContract]
     public class CompositeType
     {
